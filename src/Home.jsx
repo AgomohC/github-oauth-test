acc660300b3be681a87d58580504b3dd2db19d02
AgomohC <chinaemerem98@yahoo.com>
2022-05-02 00:21:03 +0100

    added logout btn

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { logOut } from "./appSlice";
 import Repos from "./Repos";
 
 const mapThroughItems = (items) => {
@@ -12,10 +13,14 @@ const NoRepo = () => {
    return <h2 className="no-repo">No Public Repos Found</h2>;
 };
 const Home = () => {
-   const { repo, loading } = useSelector((state) => state.app);
+   const { repo } = useSelector((state) => state.app);
+   const dispatch = useDispatch();
 
    return (
       <section id="home-section">
+         <button className="logout-btn" onClick={dispatch(logOut())}>
+            Log Out
+         </button>
          {repo.length !== 0 ? mapThroughItems(repo) : <NoRepo />}
       </section>
    );
